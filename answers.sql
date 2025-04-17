07982c4ce10e30eb6296ba3bbb5576deaf233a51
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-04-17 23:15:27 +0300

    update

--- answers.sql
+++ answers.sql
@@ -8,11 +8,12 @@ CREATE TABLE student (
 -- Question 2: Insert 3 records into the 'student' table
 INSERT INTO student (id, fullName, age)
 VALUES
-    (1, 'John Doe', 18),
-    (2, 'Jane Smith', 19),
-    (3, 'Alice Brown', 21);
+    (1, 'Timothy Khalayi', 18),
+    (2, 'Jane simiyu', 19),
+    (3, 'Alice  wanjala', 21);
 
 -- Question 3: Update the age of the student with ID 2 to 20
 UPDATE student
 SET age = 20
 WHERE id = 2;
+
